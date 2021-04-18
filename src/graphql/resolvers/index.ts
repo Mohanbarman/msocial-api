6388cc656403de70c630a9ba65a15f38e6c81c05
mohanbarman <mohanbarman9711@gmail.com>
2021-04-18 22:07:16 +0530

    Adding register resolver

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,8 +1,12 @@
 import getPosts from './getPosts';
+import register from './register';
 
 const resolvers = {
     Query: {
         getPosts,
+    },
+    Mutation: {
+        register,
     }
 }
 
