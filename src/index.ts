d93454e6ef24b30bc93ccfa26ff6384222ae16d3
mohanbarman <mohanbarman9711@gmail.com>
2021-04-18 18:20:39 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
+
+async function startServer() {
+    // Loading environment variables from .env file
+    dotenv.config();
+    const databse_url: string | undefined = process.env["DATABASE_URL"];
+    if (!databse_url) throw "DATABASE_URL is not present in the environment variables";
+
+    await mongoose.connect(databse_url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('🗄️  Connected to the databse');
+
+    const server = new ApolloServer({ typeDefs, resolvers });
+    const serverInfo = await server.listen({ port: 4000 });
+
+    console.log(`🚀 Server running on ${serverInfo.url}`);
+}
+
+startServer();
