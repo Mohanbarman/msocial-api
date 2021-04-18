2e87cae68d48fcd7259bc663050edd158e77edad
mohanbarman <mohanbarman9711@gmail.com>
2021-04-18 22:59:05 +0530

    Adding UserType

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,17 @@
-type RegisterInputType = {
+type RegisterUserInputType = {
+    firstName: String,
+    lastName: String,
     username: String,
     password: String,
     confirmPassword: String,
     email: String,
+}
+
+type UserType = {
+    firstName: string,
+    lastName: string,
+    profilePicture: string,
+    email: string,
+    createdAt?: string,
+    updatedAt?: string,
 }
