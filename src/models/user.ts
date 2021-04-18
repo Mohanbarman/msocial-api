a0a67ad3f9a54dd928b1592e409f2631392cb6c5
mohanbarman <mohanbarman9711@gmail.com>
2021-04-18 22:58:34 +0530

    Defining interface for userSchema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,25 @@
-import {model, Schema} from 'mongoose';
+import { model, Schema, Document } from 'mongoose';
 
-const userSchema = new Schema({
+interface UserInterface extends Document {
+    firstName: string,
+    lastName: string,
+    username: string,
+    profilePicture: string,
+    email: string,
+    password: string,
+    createdAt?: string,
+    updatedAt?: string,
+};
+
+const userSchema = new Schema<UserInterface>({
     firstName: String,
     lastName: String,
     profilePicture: String,
-    email: String,
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: String,
     createdAt: { type: Date, default: Date.now },
     updatedAt: Date,
 })
 
-export default model('user', userSchema);
+export default model<UserInterface>('user', userSchema);
