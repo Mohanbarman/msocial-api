60fdf44f292a0da762fcea429cd6d00b214d4757
mohanbarman <mohanbarman9711@gmail.com>
2021-04-19 10:46:52 +0530

    Removing: deprecated warnings

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,23 @@ import { ApolloServer, gql } from 'apollo-server';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-import {resolvers, typeDefs} from './graphql';
+import { resolvers, typeDefs } from './graphql';
 
 
 async function startServer() {
     // Loading environment variables from .env file
     dotenv.config();
+
     const databse_url: string | undefined = process.env["DATABASE_URL"];
     if (!databse_url) throw "DATABASE_URL is not present in the environment variables";
 
-    await mongoose.connect(databse_url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(databse_url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
     console.log('🗄️  Connected to the databse');
 
     const server = new ApolloServer({ typeDefs, resolvers });
     const serverInfo = await server.listen({ port: 4000 });
 
-    console.log(`🚀 Server running on ${serverInfo.url}`);
+    return `🚀 Server running on ${serverInfo.url}`;
 }
 
-startServer();
+startServer().then(console.log).catch(console.log);
