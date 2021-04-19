49bb0c739ccb88d94234cc3f3296e1e03c11c3e2
mohanbarman <mohanbarman9711@gmail.com>
2021-04-19 13:42:15 +0530

    Adding: adding validation in registerUser mutation

--- src/graphql/resolvers/registerUser.ts
+++ src/graphql/resolvers/registerUser.ts
@@ -1,9 +1,21 @@
 import UserModel from '../../models/user';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { validateRegisterInput } from '../../utils/validators';
+import { UserInputError, ValidationError } from 'apollo-server-errors';
 
 
 const registerUser = async (parent: any, { registerInput }: { registerInput: RegisterUserInputType }, context: any, info: any) => {
+    // validating user email and username
+    const { valid, errors } = validateRegisterInput(registerInput.email, registerInput.username, registerInput.password);
+    if (!valid) throw new UserInputError('RegisterInput contain errors', { errors });
+
+    const dupEmail = await UserModel.findOne({ email: registerInput.email }).exec();
+    const dupUsername = await UserModel.findOne({ username: registerInput.username }).exec();
+
+    if (dupEmail && dupUsername) throw new ValidationError("Username and email already taken");
+    if (dupEmail) throw new ValidationError("Email already taken");
+    if (dupUsername) throw new ValidationError("Username already taken");
+
     // Hashing password with 12 rounds
     const password = await bcrypt.hash(registerInput.password, 12);
 
@@ -16,18 +28,9 @@ const registerUser = async (parent: any, { registerInput }: { registerInput: Reg
         updatedAt: new Date().toISOString(),
     })
 
-    const res:any = await user.save();
+    const res: any = await user.save();
 
-    const jwtToken = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username,
-    }, `${process.env['SECRET_KEY']}`, { expiresIn: "1h" });
-
-    return {
-        ...res._doc,
-        token: jwtToken,
-    };
+    return user;
 }
 
 export default registerUser;
