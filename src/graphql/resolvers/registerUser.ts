51ec50bb478ffd361645111841ba4822f4b1895d
mohanbarman <mohanbarman9711@gmail.com>
2021-04-19 16:37:28 +0530

    Adding: sending token with signup

--- src/graphql/resolvers/registerUser.ts
+++ src/graphql/resolvers/registerUser.ts
@@ -1,14 +1,17 @@
-import UserModel from '../../models/user';
 import bcrypt from 'bcrypt';
-import { validateRegisterInput } from '../../utils/validators';
 import { UserInputError, ValidationError } from 'apollo-server-errors';
+
+import UserModel from '../../models/user';
+import { validateRegisterInput } from '../../utils/validators';
+import createToken from '../../utils/createToken';
 
 
 const registerUser = async (parent: any, { registerInput }: { registerInput: RegisterUserInputType }, context: any, info: any) => {
-    // validating user email and username
+    // validating user email, username, password
     const { valid, errors } = validateRegisterInput(registerInput.email, registerInput.username, registerInput.password);
     if (!valid) throw new UserInputError('RegisterInput contain errors', { errors });
 
+    // checking of email and username already exists in the database
     const dupEmail = await UserModel.findOne({ email: registerInput.email }).exec();
     const dupUsername = await UserModel.findOne({ username: registerInput.username }).exec();
 
@@ -16,9 +19,11 @@ const registerUser = async (parent: any, { registerInput }: { registerInput: Reg
     if (dupEmail) throw new ValidationError("Email already taken");
     if (dupUsername) throw new ValidationError("Username already taken");
 
+    // registration process starts from here
     // Hashing password with 12 rounds
     const password = await bcrypt.hash(registerInput.password, 12);
 
+    // creating user model with input data
     const user = new UserModel({
         firstName: registerInput.firstName,
         lastName: registerInput.lastName,
@@ -28,9 +33,14 @@ const registerUser = async (parent: any, { registerInput }: { registerInput: Reg
         updatedAt: new Date().toISOString(),
     })
 
-    const res: any = await user.save();
+    // add user to mongodb collection or throw an error if any exception occurs
+    try { await user.save() }
+    catch (e) { throw new ValidationError(e) }
 
-    return user;
+    // generating jwt token
+    const token = createToken(user.id, user.email, user.username);
+
+    return { user, token };
 }
 
 export default registerUser;
